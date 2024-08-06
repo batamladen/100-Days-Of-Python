a7a737ee74b2f70df65b3378c7259a2e51cd31d1
Mladen Nikolov <117394324+batamladen@users.noreply.github.com>
2024-08-06 13:51:55 +0200

    Add files via upload

---
+++ Day22/ball.py
@@ -0,0 +1,36 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.move_speed = 0.005
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 1
+
+    def bounce_y(self):
+        self.y_move *= -1
+        self.move_speed *= 1
+
+    def reset_position(self):
+        self.home()
+        self.move_speed = 0.005
+        self.bounce_x()
+
+
+
+
+
+
